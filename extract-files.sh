c128480c48da8ce9909f75d8064adda7360d0716
Marc Bourgoin <themard85@gmail.com>
2024-10-19 22:54:09 -0600

    rtwo: Modify MotCamera4.apk automatically during extract
    https://review.lineageos.org/q/topic:%22extract-utils-apktool%22
    
    Change-Id: I9431d02b5202c25d5c9d9274fecba8c2cd6c4419

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,27 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+function blob_fixup() {
+    case "${1}" in
+        product/priv-app/MotCamera4/MotCamera4.apk)
+            apktool_patch "${2}" "$MY_DIR/MotCamera4-patches"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
